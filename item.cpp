9b92e2b7fe260a6e590d8661a4f5ec56d9f52680
richardzhang121 <richard.zhang121@gmail.com>
2017-02-01 14:28:28 -0800

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,8 +1,10 @@
+//Item file that sets methods available
 #include "item.h"
 #include <iostream>
 
 using namespace std;
 
+//establishes name
 item::item(const char* name){
   this->name=strdup(name);
   
